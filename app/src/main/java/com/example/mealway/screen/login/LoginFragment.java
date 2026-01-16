236058121982625c143dd7cebd7d5b3487e3fe76
Ahmed Mohamed <ahmedmonataf@gmail.com>
2026-01-16 18:39:58 +0200

    finish ui for login and register

--- app/src/main/java/com/example/mealway/screen/login/LoginFragment.java
+++ app/src/main/java/com/example/mealway/screen/login/LoginFragment.java
@@ -7,24 +7,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.mealway.R;
 import com.example.mealway.data.repository.AuthRepositoryImpl;
 import com.example.mealway.screen.MainActivity;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginFragment extends Fragment implements LoginView {
 
     private LoginPresenter presenter;
-    private EditText emailEditText, passwordEditText;
-    private Button loginButton;
+    private TextInputEditText usernameEditText, passwordEditText;
+    private MaterialButton loginButton;
+    private TextView registerLink;
 
     @Nullable
     @Override
@@ -34,22 +37,27 @@ public class LoginFragment extends Fragment implements LoginView {
 
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
-        emailEditText = view.findViewById(R.id.email_edit_text);
-        passwordEditText = view.findViewById(R.id.password_edit_text);
-        loginButton = view.findViewById(R.id.login_button);
+        usernameEditText = view.findViewById(R.id.usernameInput);
+        passwordEditText = view.findViewById(R.id.passwordInput);
+        loginButton = view.findViewById(R.id.loginButton);
+        registerLink = view.findViewById(R.id.registerLink);
 
         presenter = new LoginPresenter(this, new AuthRepositoryImpl());
 
         loginButton.setOnClickListener(v -> {
-            String email = emailEditText.getText().toString().trim();
+            String username = usernameEditText.getText().toString().trim();
             String pass = passwordEditText.getText().toString().trim();
 
-            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)) {
-                Toast.makeText(getContext(), "Please enter email & password", Toast.LENGTH_SHORT).show();
+            if(TextUtils.isEmpty(username) || TextUtils.isEmpty(pass)) {
+                Toast.makeText(getContext(), "Please enter username & password", Toast.LENGTH_SHORT).show();
             } else {
-                presenter.login(email, pass);
+                presenter.login(username, pass);
             }
         });
+
+        registerLink.setOnClickListener(v ->
+                NavHostFragment.findNavController(this).navigate(R.id.action_login_to_register)
+        );
 
         return view;
     }
@@ -57,9 +65,7 @@ public class LoginFragment extends Fragment implements LoginView {
     @Override
     public void showLoginSuccess() {
         Toast.makeText(getContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-        if(getActivity() instanceof MainActivity){
-          //  ((MainActivity)getActivity()).loadFragment(new com.example.mealway.screen.homeactivity.HomeFragment());
-        }
+        NavHostFragment.findNavController(this).navigate(R.id.action_login_to_home);
     }
 
     @Override
