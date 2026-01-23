ec714cc24bed3189da41fd3482bb974f92e5488d
Ahmed Mohamed <ahmedmonataf@gmail.com>
2026-01-23 16:34:04 +0200

    make alert and solve problem in profile

--- app/src/main/java/com/example/mealway/screen/mealdetails/presenter/MealDetailsPresenter.java
+++ app/src/main/java/com/example/mealway/screen/mealdetails/presenter/MealDetailsPresenter.java
@@ -8,5 +8,8 @@ public interface MealDetailsPresenter {
     void addToFavorites(Meal meal);
     void removeFromFavorites(Meal meal);
     void addAppointment(Meal meal, long timestamp);
+    void onFavoriteClicked(Meal meal, boolean isFavorite);
+    void onPlanClicked();
+    void onVideoClicked(String youtubeUrl);
     void onDestroy();
 }
--- app/src/main/java/com/example/mealway/screen/mealdetails/view/MealDetailsView.java
+++ app/src/main/java/com/example/mealway/screen/mealdetails/view/MealDetailsView.java
@@ -6,6 +6,11 @@ public interface MealDetailsView {
     void showMealDetails(Meal meal);
     void showFavoriteStatus(boolean isFavorite);
     void showMessage(String message);
+    void showSuccess(String message);
+    void showError(String message);
     void showLoading();
     void hideLoading();
+    void navigateToLogin();
+    void showDatePicker();
+    void prepareVideo(String videoId);
 }
